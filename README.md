db507f2f7b9c3f5ff75ed361570b7479c099a1c6
pranavaselva <145452022+pranavaselva@users.noreply.github.com>
2023-10-14 07:32:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-js-Operators
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/pranavaselva/web-platform-js-Operators)
---
+++ script.js
@@ -0,0 +1,55 @@
+var x = 5;
+var y = 2;
+
+// Challenge 1: Perform the addition operation (add x and y) and print the answer in the console.
+console.log(x + y);
+// Challenge 2: Perform the Subtraction operation (subtract y from x) and print the answer in the console.
+console.log(y - x);
+
+// Challenge 3: Perform Multiplication operation (multiply x from y) and print the answer in console.
+console.log(x * y);
+// Challenge 4: Perform Division operation (Divide x by y) and print the answer in console.
+console.log(x / y);
+// Challenge 5: perform division operation (divide x by y) and print the remainder in console.
+console.log(x % y);
+// Challenge 6: Perform Exponent operation and print the answer in console.
+console.log(x ** y);
+var z = 20;
+// Challenge 7: calculate "x" to the power of the "y" and print the answer in console.
+console.log(y ** x);
+// Challenge 8: Decrement the value of the variable "z" by 1 and print the z value in the console.
+console.log(z--);
+var a = 20;
+
+var b = 10;
+
+var c = 15;
+
+var d = 20;
+
+// Challenge 9: Perform Addition assignment operation to the variable a;
+console.log(a + b);
+// Challenge 10: Perform 	Subtraction assignment operation to the variable b;
+console.log(b - c);
+// Challenge 11: Perform 	Multiplication assignment operation to the variable c;
+console.log(c * b);
+// Challenge 12: Perform 	Division assignment operation to the variable c;
+console.log(c / b);
+// Challenge 13: Perform 2 Strict equality Comparison operations, in which one should return true and one should return false.
+console.log(a === d);
+console.log(b === c);
+// Challenge 14: Perform 2 Strict-non-equality Comparison operations, in which one should return true and one should return false.
+console.log(b !== c);
+console.log(a !== d);
+// Challenge 15: Perform 2 Less than Comparison operations, in which one should return true and one should return false.
+console.log(b < a);
+console.log(a < b);
+// Challenge 16: Perform 2 Greater than Comparison operations, in which one should return true and one should return false.
+console.log(a > b);
+console.log(c > a);
+// Challenge 17: Perform 2 Less than or equal to Comparison operations, in which one should return true and one should return false.
+console.log(b <= a);
+console.log(a <= b);
+// Challenge 18: Perform 2 Greater than or equal to Comparison operations, in which one should return true and one should return false.
+console.log(a >= b);
+console.log(c >= d);
